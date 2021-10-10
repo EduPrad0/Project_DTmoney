19bb6ca76b4a49b5f7fcd80046e9cbf08d5cbcf6
Eduardo Do Prado Cardoso <dpradoeduardo@gmail.com>
2021-10-10 20:35:31 -0300

    First commit and push github

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+
+interface Transaction {
+    id: string;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
+export function TransactionTable(){
+    const [ transactions, setTransactions] = useState<Transaction[]>([]);
+
+    useEffect(() => {
+        api.get('transactions')
+            .then(response => setTransactions(response.data['transactions']))
+    }, [])
+
+
+    return(
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                  {transactions.map(transaction => {
+                    return(
+                        <tr key={transaction.id}>
+                            <td> {transaction.title } </td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL',
+
+                                
+                                }
+                                ).format(transaction.amount)} 
+                            </td>
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))} 
+                            </td>
+                        </tr>
+                    );
+                    })}
+                    
+                    
+                    
+                </tbody>
+
+            </table>
+        </Container>
+    );
+
+}
